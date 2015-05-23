a4bfb2e2e7a885eff87a4bc0a720d892143acbca
Christoph Dorn <christoph@christophdorn.com>
2015-05-22 19:57:30 -0700

    wip

--- postdeploy.js
+++ postdeploy.js
@@ -5,3 +5,13 @@ console.log("Running postdeploy NodeJS module!", process.env);
 
 // TODO: Use 'org.pinf.genesis.lib' to laod config & then process postdeploy for package.
 
+
+require('org.pinf.genesis.lib/lib/api').forModule(require, module, function (API, exports) {
+
+	// TODO: init env from package.json overlayed by package.service.json
+
+	console.log("BOOTED postdeploy module with API!");
+
+
+});
+
